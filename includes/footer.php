ccc30145db34daa6ff91d465ddf6cad284edd018
Willian Danilo <55411079+willdanilomendes@users.noreply.github.com>
2020-07-14 18:33:04 -0300

    add : footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,92 @@
 <footer>
-  <div class="main">
+  <main class="redesocial">
     <div class="container">
       <div class="row">
         <div class="col-md-3">
-          <h4>Acompanhe nossas redes e assista à aulas gratuitas</h4>
+          <h4 class="redesocial__title">Acompanhe nossas redes e assista à aulas gratuitas</h4>
         </div>
         <div class="col-md-3">
           <div class="d-flex align-items-center">
             <a href="">
-              <img src="" alt="">
-              <span>@neojus</span>
+              <img src="<?php echo U?>src/img/icons/icon-youtube.png" alt="">
+              <span class="redesocial__link">@neojus</span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex align-items-center">
+            <a href="">
+              <img src="<?php echo U?>src/img/icons/icon-linkedin.png" alt="">
+              <span class="redesocial__link">@neojus</span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex align-items-center">
+            <a href="">
+              <img src="<?php echo U?>src/img/icons/icon-instagram.png" alt="">
+              <span class="redesocial__link">@neojus</span>
             </a>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </main>
+  <main class="contato">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-12">
+          <h5 class="contato__title">Deixe seu contato e receba dicas jurídicas que farão diferença no seu dia-a-dia,
+            além de informações sobre
+            nossos cursos de direto.</h5>
+        </div>
+        <div class="col-md-6 col-12">
+          <form action="" method="post">
+            <div class="form-group">
+              <input type="text" name="Nome" placeholder="Digite seu nome"
+                class="form-control form-control-lg form__default" id="">
+            </div>
+            <div class="row">
+              <div class="col-md-8 col-12">
+                <div class="form-group">
+                  <input type="mail" name="Nome" placeholder="Digite seu E-mail"
+                    class="form-control form-control-lg form__default" id="">
+                </div>
+              </div>
+              <div class="col-md-4 col-12">
+                <div class="form-group">
+                  <input type="mail" name="Nome" placeholder="Celular"
+                    class="form-control form-control-lg form__default" id="">
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn__enviar float-right">Enviar <img
+                  src="<?php echo U?>src/img/icons/icon-enviar.png" alt=""></button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </main>
+  <main class="parceiros">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h4>Parceiros <br> NeoJus</h4>
+        </div>
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-3">
+
+        </div>
+      </div>
+    </div>
+  </main>
 </footer>
 
 
